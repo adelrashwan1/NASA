aa4a1ab23a96acce0e0f22b240ae16bfaef55919
ADEL <151628404+adelrashwan1@users.noreply.github.com>
2023-12-15 18:38:32 +0200

    Add files via upload

--- js/js.js
+++ js/js.js
@@ -6,3 +6,19 @@ let imgs = document.querySelector(`.img1`);
 function foods(food){
     imgs.src = food;
 }
+var nam = [];
+document.getElementById("save").addEventListener("click", function() {
+   var sname = document.getElementById("name").value;
+   nam.push(sname);
+   var newP = document.createElement("p");
+   newP.textContent = sname;
+   document.getElementById("names1").appendChild(newP);
+   document.getElementById("name").value = "";
+});
+document.getElementById("dleet").addEventListener("click", function() {
+   var sname = document.getElementById("name").value;
+   nam.pop(sname);
+   var namesContainer = document.getElementById("names1");
+   namesContainer.removeChild(namesContainer.lastChild);
+   document.getElementById("name").value = "";
+});
